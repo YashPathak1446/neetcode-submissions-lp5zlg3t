5252f86dfeab879c36969c5005f2761f36884b6b
Yash Pathak <64624038+YashPathak1446@users.noreply.github.com>
2026-07-03 15:21:09 -0700

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        def dfs(i, j, visited):
+            if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or board[i][j] == "X" or (i,j) in visited:
+                return
+            visited.add((i,j))
+            dfs(i, j+1, visited)
+            dfs(i+1, j, visited)
+            dfs(i, j-1, visited)
+            dfs(i-1, j, visited)
+            return
+        
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                if board[i][j] == "O":
+                    visited = set()
+                    dfs(i, j, visited)
+                    contains_edge = False
+                    for x, y in visited:
+                        if x == 0 or x == len(board) - 1 or y == 0 or y == len(board[0]) - 1:
+                            contains_edge = True
+                            break
+                    if not contains_edge:
+                        for x, y in visited:
+                            board[x][y] = "X"
+        return 
