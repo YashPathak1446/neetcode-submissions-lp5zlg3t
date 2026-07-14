d2eeba28a32495fb9b1cf311e0da30009f56866f
Yash Pathak <64624038+YashPathak1446@users.noreply.github.com>
2026-07-13 21:43:39 -0700

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = dict()
+        # track as min cost to reach top of the staircase
+
+        def dfs(curr_step):
+            if curr_step in memo:
+                return memo[curr_step]
+            if curr_step == len(cost):
+                return 0
+            if curr_step > len(cost):
+                return 0
+            memo[curr_step] = cost[curr_step] + min(dfs(curr_step + 1), dfs(curr_step + 2))
+            return memo[curr_step]
+        
+        return min(dfs(0), dfs(1))
